ef801476cf720d970044c2bf67f6757889c7cec3
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-27 17:56:12 -0400

    First express server

---
+++ block-BNaabk/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+
+app.get(`/`, (req, res) => {
+  console.log(req);
+  console.log(res);
+  res.send(`<h1>Welcome in HTML type response</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log(`Server running at port 3000`);
+});
