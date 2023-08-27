18e6e805c929e1faa12490e4b254361cacb9c846
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-27 18:32:53 -0400

    server.js

--- block-BNaabk/server.js
+++ block-BNaabk/server.js
@@ -4,7 +4,6 @@ var app = express();
 app.get(`/`, (req, res) => {
   res.send(`<h1>Welcome in HTML type response</h1>`);
 });
-
 app.listen(3000, () => {
   console.log(`Server running at port 3000`);
 });
