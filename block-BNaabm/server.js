d806584a1215bcdf272fc948f532bcc99589de6c
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-27 21:01:43 -0400

    custom middleware passing data

---
+++ block-BNaabm/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var app = express();
+
+app.use('/', (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send(`Welcome!!`);
+});
+
+app.listen(4000, () => {
+  console.log(`Server listening on port 4000`);
+});
